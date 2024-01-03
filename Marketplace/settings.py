fbfaa3551096f7cd194390b5d61b92c4d762a68c
Abdxl <adaromosu@gmail.com>
2024-01-03 14:01:04 +0100

    updated the requirements, follow the instruction to use the postgresql database for this project, make sure to keep secrets safe

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -13,22 +13,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.db.utils import OperationalError
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lu=z8ne=b6im4)a+6+qv34h7*p$_%tw+9n^i%og82-pnxdzc9g"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
