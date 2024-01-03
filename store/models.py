c8a097b64a2f6e83c8f3f529f275f3c6cdb5eef8
A1bdul <73336016+A1bdul@users.noreply.github.com>
2024-01-03 21:34:00 +0000

    updated frontend to django routing

--- store/models.py
+++ store/models.py
@@ -10,9 +10,70 @@ from collections.abc import Iterable
 from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
 from django.http import HttpRequest
-from Marketplace.settings import BASE_DIR
+from django.conf import settings
 
 # Create your models here
+
+from django.contrib.auth.models import AbstractUser
+
+
+
+# Create your models here.
+class User(AbstractUser):
+    """Django's default user model is extended for this wish list option for active user,
+    Shipping Address for user is saved from checkout process
+    """
+
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.CharField(max_length=200)
+    wish_list = models.ManyToManyField("Product", blank=True, null-True)
+    address_to = models.ForeignKey(
+        "ShippingAddress", on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.email
+
+
+class ShippingAddress(models.Model):
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    mobile_number = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=200)
+    zip = models.IntegerField(blank=True, null=True)
+
+
+class OrderedItem(models.Model):
+    item = models.ForeignKey(
+        "Product", on_delete=models.CASCADE, related_name="ordered_item", null=True
+    )
+    quantity = models.IntegerField(default=1)
+    consumer = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def ordered_item_total(self):
+        return self.item.price * self.quantity
+
+
+class Cart(models.Model):
+    items = models.ManyToManyField("OrderedItem", blank=True)
+    transaction_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    completed = models.BooleanField(default=False)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    processing = models.BooleanField(default=False)
+    consumer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    notes = models.TextField(blank=True)
+    shipping_to = models.ForeignKey(
+        ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def cart_total(self):
+        total = sum([price.ordered_item_total() for price in self.items.all()])
+        return total
+
+    def quantity_total(self):
+        return self.items.count()
 
 
 class ChoiceArrayField(ArrayField):
@@ -65,31 +126,7 @@ class Product(models.Model):
     ):
         self.name = self.name.title()
         super(Product, self).save(*args, **kwargs)
-        path = os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__), "..", f"products/{self.name.lower()}"
-            )
-        )
-        if not os.path.exists(path=path):
-            os.makedirs(path)
-        html = render_to_string("product.html", {"instance": self})
-        with open(path, "w") as file:
-            file.write(html)
-
-    def delete(
-        self, using: Any = ..., keep_parents: bool = ...
-    ) -> tuple[int, dict[str, int]]:
-        path = os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__), "..", f"products/{self.name.lower()}"
-            )
-        )
-        if not os.path.exists(path=path):
-            os.makedirs(path)
-        if os.path.exists(path) and os.path.isdir(path):
-            shutil.rmtree(path)
-        return super().delete(using, keep_parents)
-
+       
     @admin.display(description="")
     def image_display(self):
         if not self.image:
@@ -129,20 +166,7 @@ class Category(models.Model):
     ) -> None:
         self.name = self.name.title()
         super().save(*args, **kwargs)
-        path = os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__), "..", f"product-category/{self.name.lower()}"
-            )
-        )
-        if not os.path.exists(path=path):
-            os.makedirs(path)
-        html = render_to_string(
-            "layouts/category.html",
-            {"instance": self, "request": HttpRequest},
-        )
-        with open(os.path.join(path, "index.html"), "w") as file:
-            file.write(html)
-
+        
     def __str__(self) -> str:
         return self.name
 
